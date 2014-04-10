607097613fba9410ee06c94be2043e97fadf1558
LTChaseJohnson <chasevjohnson@gmail.com>
2014-04-10 14:58:43 -0400

    Lesson 11 Update Panel Complete

--- Lesson 11 Source Vortex panel method.py
+++ Lesson 11 Source Vortex panel method.py
@@ -28,11 +28,12 @@ class Panel:
     def __init__(self,xa,ya,xb,yb):
         self.xa,self.ya = xa,ya                       #First endpoint of panel
         self.xb,self.yb = xb,yb                       #Second endpoint of panel
-        self.xc,self.yc = sqrt((xb-xa)**2+(yb-ya)**2) #Control center point
+        self.xc,self.yc = (xa+xb)/2,(ya+yb)/2         #Control center point
+        self.length = sqrt((xb-xa)**2+(yb-ya)**2)
         
         #Sets the orientation of the panel
-        if (xb-xa<=0.): self.beta = acos((yb-a)/self.length)
-        elif (xb-xa>0/): self.beta = pi+acos(-(yb-a)/self.length)
+        if (xb-xa<=0.): self.beta = acos((yb-ya)/self.length)
+        elif (xb-xa>0.): self.beta = pi+acos(-(yb-ya)/self.length)
         
         #Sets the location of the panel
         if (self.beta<=pi): self.loc = 'Top Surface'
@@ -58,18 +59,33 @@ def definePanels(N,xp,yp):
         while (I<len(xp)-1):
             if (xp[I]<=x[i]<=xp[I+1] or xp[I+1]<=x[i]<=xp[I]): break
             else: I += 1
-        a = (p[I+1]-yp[I])/(xp[I+1]-xp[I])
+        a = (yp[I+1]-yp[I])/(xp[I+1]-xp[I])
         b = yp[I+1]-a*xp[I+1]
         y[i] = a*x[i]+b
     y[N] = y[0]
     
-    panel = np.empy(N,dype=object)
+    panel = np.empty(N,dtype=object)
     for i in range(N):
         panel[i] = Panel(x[i],y[i],x[i+1],y[i+1])
     
     return panel
 
+N = input('Enter number of panels: ')
+panel = definePanels(N,xp,yp)
 
-
+valX,valY =0.1,0.2
+xmin,xmax = min([p.xa for p in panel]),max([p.xa for p in panel])
+ymin,ymax = min([p.ya for p in panel]),max([p.ya for p in panel])
+xStart,xEnd = xmin-valX*(xmax-xmin),xmax+valX*(xmax-xmin)
+yStart,yEnd = ymin-valY*(ymax-ymin),ymax+valY*(ymax-ymin)
+size = 10
+plt.figure(figsize=(size,(yEnd-yStart)/(xEnd-xStart)*size))
+plt.grid(True)
+plt.xlabel('x',fontsize=16)
+plt.ylabel('y',fontsize=16)
+plt.xlim(xStart,xEnd)
+plt.ylim(yStart,yEnd)
+plt.plot(xp,yp,'k-',linewidth=2)
+plt.plot(np.append([p.xa for p in panel],panel[0].xa),np.append([p.ya for p in panel],panel[0].ya),'r-',linewidth=1,marker='o',markersize=6)
 
 plt.show()
