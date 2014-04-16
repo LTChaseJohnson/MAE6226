fc343a825c41e38a186acd1ae32bcff719a42cd4
LTChaseJohnson <chasevjohnson@gmail.com>
2014-04-16 14:16:12 -0400

    Updated Lesson 11 Solved Linear Equation and Pressure Coefficient

--- Lesson 11 Source Vortex panel method.py
+++ Lesson 11 Source Vortex panel method.py
@@ -159,6 +159,38 @@ def buildRHS(p,fs):
     B[N] = -fs.Uinf*(sin(fs.alpha-p[0].beta)+sin(fs.alpha-p[N-1].beta))
     return B
 
+# Building the linear equation and solving
+A = buildMatrix(panel)
+B = buildRHS(panel,freestream)
+
+var = np.linalg.solve(A,B)
+for i in range(len(panel)):
+    panel[i].sigma = var[i]
+gamma = var[-1]
+
+# Defining tangent velocity for pressure coefficient
+def getTangentVelocity(p,fs,gamma):
+    N = len(p)
+    A = np.zeros((N,N+1),dtype=float)
+    for i in range(N):
+        for j in range(N):
+            if (i!=j):
+                A[i,j] = 0.5/pi*I(p[i].xc,p[i].yc,p[j],-sin(p[i].beta),cos(p[i].beta))
+                A[i,N] = 0.5/pi*I(p[i].xc,p[i].yc,p[j],cos(p[i].beta),sin(p[i].beta))
+    B = fs.Uinf*np.sin([fs.alpha-pp.beta for pp in p])
+    var = np.empty(N+1,dtype=float)
+    var = np.append([pp.sigma for pp in p],gamma)
+    vt = np.dot(A,var)+B
+    for i in range(N):
+        p[i].vt=vt[i]
+getTangetVelocity(panel,freestream,gamma)
+
+#Defining pressure coefficient based on tangent velocity
+def getPressureCoeff(p,fs):
+    for i in range(len(p)):
+        p[i].Cp = 1 - (p[i].vt/fs.Uinf)**2
+getPressureCoeff(panel,freestream)
+
 
 
 plt.show()
